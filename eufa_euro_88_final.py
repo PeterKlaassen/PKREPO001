4d5ba774589ede3651ad1d1c050c30ff8f24dee0
PeterKlaassen <77883244+PeterKlaassen@users.noreply.github.com>
2021-01-29 13:14:58 +0100

    Add files via upload

---
+++ eufa_euro_88_final.py
@@ -0,0 +1,50 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+player0 = 'Ruud Gullit '
+name = ('player0')
+
+player1 = 'Marco van Basten '
+name = ('player1')
+
+goal_0 = 32
+name = ('goal_0')
+
+goal_1 = 54
+name = ('goal_1')
+
+scorers  = (player0 +str(goal_0), player1 +str(goal_1))
+name = (scorers)
+
+#report = (f'{anwser0}scored in the {anwser1}th minute.\n{anwser2}scored in the {anwser3}th minute.')
+#name = report
+anwser0 = (player0)
+name = (anwser0)
+anwser1 = (goal_0)
+name = (anwser1)
+anwser2 = (player1)
+name = (anwser2)
+anwser3 = (goal_1)
+name = (anwser3)
+report = (f'{anwser0}scored in the {anwser1}th minute.\n{anwser2}scored in the {anwser3}th minute.')
+name = report
+#print (f'{anwser0}scored in the {anwser1}th minute.\n{anwser2}scored in the {anwser3}th minute.')
+print (report)
+
+#player1 scored in the goal0 th minute
+
+#anwser = player0 qa = f' {answer}', /n 
+#<scorer_name> scored in the <when_they_scored>th minute
+#<scorer_name> scored in the <when_they_scored>th minute
+#print (player0 +str(goal_0), player1 + str(goal_1))
+#print (scorers)
+#print (player0 + str(goal_0))
+
+
+#answer = goal_0
+#qa = f'The answer is.. {answer}' /n
+
+#<scorer_name> scored in the <when_they_scored>th minute
+#<scorer_name> scored in the <when_they_scored>th minute

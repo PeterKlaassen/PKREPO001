b4c4cdb22d332e6d65991ba1eb5dfedb8c66a44a
PeterKlaassen <77883244+PeterKlaassen@users.noreply.github.com>
2021-02-09 14:32:27 +0100

    Add files via upload

--- eufa_euro_88_final.py
+++ eufa_euro_88_final.py
@@ -15,62 +15,42 @@ goal_1 = 54
 scorers = player0 + " " +str(goal_0) + " , " + player1  + " " +str(goal_1)
 print(scorers)
 
-anwser0 = player0
-anwser1 = goal_0
-anwser2 = player1
-anwser3 = goal_1
-report = (f'{anwser0} scored in the {anwser1}th minute.\n{anwser2} scored in the {anwser3}th minute.')
+report = f'{player0} scored in the {goal_0}th minute.\n{player1} scored in the {goal_1}th minute.'
 print(report)
-
-#testing print(player0 +str(goal_0), player1 + str(goal_1))
-#testing print(player0 + str(goal_0))
-
 
 #Part 2
 
 
 player2 = "Ronald Koeman"
 
-first_name = player2[:6]
-find = player2.find("R")
+find_space = player2.find(" ")
+first_name = player2[0:find_space]
 
-#testing print (first_name)
-#testing print (find, first_name)
-#testing print (find)
+print (first_name)
 
-last_name_len = player2[7:]
-find = player2.find("Koeman")
-#testing print(len(last_name_len))
-#testing print(find, len(last_name_len))
-#testing print(find)
+last_name = player2[-find_space:]
+last_name_len = len(last_name)
 
-name_short = player2[:1]
-find = player2.find("R")
-#testing print (name_short + ". " + last_name_len)
-#testing print (find, name_short + ". " + last_name_len)
-#testing print (find)
-players_fn = player0[:4]
-bang = "!  "
+print(last_name)
+print(last_name_len)
+
+name_short = player2[0]
+
+print (name_short + ". " + last_name)
+
+players_fn = player2[0:find_space]
+bang = "! "
 
 first_name_with = players_fn + bang
 
 players_fn_len = (len(players_fn))
 chant = players_fn_len* first_name_with
-string_len = (len(chant))
 
-space_at_end = chant[-1]
-
-good_chant = chant[0:26]
+space_at_end = chant[:-1]
+good_chant = space_at_end
 
 print (good_chant)
 
-
-#testing print(chant[0:23])
-#testing print (space_at_end)
-#testing print(players_fn_len)
-#testing print(string_len)
-#testing print (chant[:24]
-#testing print (players_fn)
 
 
 

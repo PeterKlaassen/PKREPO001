e0b12c933849c643f1f2028420fa06b8b1dcb2ec
PeterKlaassen <77883244+PeterKlaassen@users.noreply.github.com>
2021-04-06 16:16:33 +0200

    Add files via upload

--- Assignment_Arguments.py
+++ Assignment_Arguments.py
@@ -6,9 +6,12 @@ __human_name__ = 'arguments'
 # Add your code after this line
 # Greet
 
+
 def greet(name, template="Hello, <name>!"):
     template.replace("<name>", name)
     return(template.replace("<name>", name))
+
+
 print(greet("David"))
 print(greet("Doc"))
 print(greet("Python"))
@@ -17,35 +20,38 @@ print(greet("Peet", "Power to you!: " "<name>"))
 
 # Force
 
-def force(mass:float, body="earth"):
+
+def force(mass, body="earth"):
     planets_dict = {
-    "earth": 9.7,
-    "sun": 274,
-    "jupiter": 24.9,
-    "neptune": 11.1,
-    "saturn": 10.4,
-    "uranus": 8.8,
-    "venus": 8.8,
-    "mars": 3.7,
-    "mercury": 3.7,
-    "moon": 1.6,
-    "pluto": 0.5
+        "earth": 9.8,
+        "sun": 274,
+        "jupiter": 24.9,
+        "neptune": 11.1,
+        "saturn": 10.4,
+        "uranus": 8.8,
+        "venus": 8.8,
+        "mars": 3.7,
+        "mercury": 3.7,
+        "moon": 1.6,
+        "pluto": 0.5
     }
-    output = mass * planets_dict.get(body)
+    G = round(planets_dict.get(body.lower()), 1)
+    output = mass * G
 
     return output
 
-print(force(59736.1, "earth"))
+
+print(force(59736.1,))
 print(force(19891.1, "sun"))
 
 
 #Pull
 
-def pull(m1,m2,r):
+def pull(m1, m2, r):
 
     G = 6.674 * 10 ** -11
     f = G * ((m1 * m2) / r ** 2)
     return(f)
-    
-print(pull(800, 1500, 3))
 
+
+print(pull(800, 1500, 3))

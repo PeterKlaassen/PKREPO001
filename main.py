620c66afefe3e2364eddb0fd77656cef032fe7fb
PeterKlaassen <77883244+PeterKlaassen@users.noreply.github.com>
2021-05-04 18:11:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ __winc_id__ = 'ae539110d03e49ea8738fd413ac44ba8'
 __human_name__ = 'files'
 
 path = os.getcwd()
-print("The current working directory is %s" % path)
+print("The current working directory is: %s" % path)
+
 
 def clean_cache():
     try:
@@ -19,33 +20,38 @@ def clean_cache():
             os.remove(file)
     if not os.path.exists('cache'):
         os.makedirs('cache')
-    else:
-        return ('cache allready exists, it is empty, move forwards')
-print(clean_cache())
+    # else:
+        # return ('cache allready exists, it is empty, move forwards')
+clean_cache()
+
+
 def cache_zip(zip_path, cache_path):
-    zip_path = r'C:/Users/HPower/AppData/Roaming/Python/Python39/Scripts/files'
-    cache_path = r'C:/Users/HPower/AppData/Roaming/Python/Python39/Scripts/files/cache'
-    files = zip_path
+    zip_path = "%s" % path
+    cache_path = "%s" % path
+    
     filename = "zip_path/data.zip"
     zip_obj = zipfile.ZipFile("data.zip", "r")
     zip_obj.extractall("cache")
     zip_obj.close()
 print(cache_zip("files", "cache"))
 
+
 def cached_files():
-    directory = r'C:/Users/HPower/AppData/Roaming/Python/Python39/Scripts/files/cache/*.*'
-    os.path.abspath('C:/Users/HPower/AppData/Roaming/Python/Python39/Scripts/files/cache')
-    cached_path_files = [os.path.join(r'C:/Users/HPower/AppData/Roaming/Python/Python39/Scripts/files/cache', file)
-                         for file in os.listdir(r'C:/Users/HPower/AppData/Roaming/Python/Python39/Scripts/files/cache')]
+    directory = "%s" % path
+    os.path.abspath(path)
+    cached_path_files = [os.path.join(r'cache', file)
+                         for file in os.listdir(r'cache')]
     cached_files = [file for file in cached_path_files if os.path.isfile(file)]
     return cached_files
 print(cached_files())
+
 
 def cached_files():
     new_list = []
     for file in os.listdir(os.path.abspath("cache")):
         new_list.append(f"{os.path.abspath('cache')}\{file}")
     return new_list
+
 
 def find_password(list):
     search_str = "password:"
@@ -55,9 +61,10 @@ def find_password(list):
             print(file_name)
 find_password(cached_files())
 
+
 def find_password(cached_files):
-    directory = r'C:/Users/HPower/AppData/Roaming/Python/Python39/Scripts/files/cache'
-    with open(r'C:/Users/HPower/AppData/Roaming/Python/Python39/Scripts/files/cache/601.txt', 'r') as read_obj:
+    directory = "%s" % path
+    with open(r'cache/601.txt', 'r') as read_obj:
         # Read all lines in the file one by one
         for line in read_obj:
             # For each line, check if line contains the string
@@ -65,6 +72,7 @@ def find_password(cached_files):
                 return line[10:38]
     return False
 print(find_password(cached_files))
+
 
 #if __name__ == '__main__':
       #print(cache_zip("files", "cache"))
